efa85eb9a0e1921acb884b29f6c6ccc6b483bd44
Brad Morgan <jbradmorg16@gmail.com>
2021-12-29 12:29:20 -0500

    initial

---
+++ main.js
@@ -0,0 +1,17 @@
+
+const person = {
+    fname: 'Brad',
+    lname: 'Morgan',
+    email: 'jbradmorg16@gmail.com',
+    hobbies: ['tech enthusiast', 'beer enthusiast', 'studying', 'working out'],
+    jobs: ['SAP Consultant', 'Taproom Server'],
+    address: {
+        street: '221 Bennett Farms Trail',
+        city: 'Acworth',
+        state: 'GA',
+        zip: '30102'
+    }
+}
+
+
+console.log(person.hobbies[0])
