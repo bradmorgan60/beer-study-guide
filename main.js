39be7f371d219b097fdd0c656c176c92511ce3b3
Brad Morgan <jbradmorg16@gmail.com>
2022-01-08 10:29:49 -0500

    hop additions

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
-for(let i = 1; i <= 10; i++) {
-    console.log(`For loop number: ${i}`)
+const todos = [
+    {
+        id: 1,
+        activity: 'study for work',
+        isComplete: false
+    },
+    {
+        id: 2,
+        activity: 'learn Javascript',
+        isComplete: true
+    },
+    {
+        id: 3,
+        activity: 'exercise',
+        isComplete: false
+    }
 
+]
+
+for(let todo of todos) {
+    if(todo.isComplete == true) {
+        console.log(`The '${todo.activity}' activity is complete`)
+    }
+    else {
+        console.log(`Get your ass in gear on '${todo.activity}'`)
+    }
+    //console.log(todo.activity, todo.isComplete)
 }
+
+
